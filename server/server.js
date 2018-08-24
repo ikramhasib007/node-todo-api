dc6b9d79968ac772afc42a8f192c5dc018755102
ikramhasib007 <ikramhasib007@gmail.com>
2018-08-24 20:18:46 +0600

    adding get route by id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
+const {ObjectID} = require('mongodb');
 
 const app = express();
 
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+    }, (err) => {
+        return res.status(400).send();
+    })
 });
 
 
